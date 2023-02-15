4e4d3f4022ce33dee387361cf75974453d5db5bd
Daria Yakkola <yakkolabest@gmail.com>
2023-02-15 18:37:43 +0300

    Задача 15

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите цифру, обозначающую день недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 1 && number <= 7)
+  if (number >= 1 && number < 6)
+  {
+    Console.WriteLine("Идем на работу :( ");
+  }
+  else
+  {
+    Console.WriteLine("ОТДЫХАЕМ!!! ");
+  }
+else
+  {
+    Console.WriteLine("Такого дня недели не существует! ");
+  }
